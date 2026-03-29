9ddae2999032a2f50ba05215dd9c0af1ab2c8d01
chandra parmar <chandra.parmar2004@gmail.com>
2026-03-29 20:23:07 +0530

    routes mounting and middlewares added

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 const express = require('express')
-
+const connectdb = require('./config/database')
+const cookieParser = require('cookie-parser')
 const app = express()
-
-
 const port =5001
 
-app.use('/',(req,res)=>{
-    res.send("hello from the server")
-})
+
+
+
+
+//database connectivity
+connectdb()
+
+
+//MIDDLEWARES
+app.use(express.json())
+app.use(cookieParser())
+
+
+//route mounting
+const authRouter = require('./routes/auth')
+const profileRouter = require('./routes/profile')
+const requestRouter = require('./routes/request')
+const userRouter = require('./routes/user')
+
+
+app.use('/api/',authRouter)
+app.use('/api/',profileRouter)
+app.use('/api/',requestRouter)
+app.use('/api/',userRouter)
+
+
+
+
+
+
+
+
+
 
 app.listen(port,()=>{
     console.log("server is running at port "+port)
